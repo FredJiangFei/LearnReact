c9abd58b12da39dee35d63c69dff3e829b981369
FredJiang <jiangf@shinetechchina.com>
2019-03-11 17:02:56 +0800

    like movie

---
+++ vidly/src/commons/like.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Like = props => {
+  const className = props.liked ? 'fa fa-heart' : 'fa fa-heart-o';
+  return (
+    <i
+      className={className}
+      style={{ cursor: 'pointer' }}
+      onClick={props.likeToggle}
+    />
+  );
+};
+
+export default Like;
--- vidly/src/components/movies.jsx
+++ vidly/src/components/movies.jsx
@@ -1,9 +1,59 @@
 import React, { Component } from 'react';
+import { getMovies } from './../services/fakeMovieService';
+import Like from '../commons/like';
 
 class Movies extends Component {
-  state = {};
+  state = {
+    movies: getMovies()
+  };
+
+  handleLike = m => {
+    const movies = [...this.state.movies];
+    const idx = movies.indexOf(m);
+    movies[idx] = { ...movies[idx] };
+    movies[idx].liked = !movies[idx].liked;
+    this.setState({ movies });
+  };
+
   render() {
-    return <h2>Movive Components</h2>;
+    return (
+      <React.Fragment>
+        <h2>Movive Components</h2>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Genre</th>
+              <th>Stock</th>
+              <th>Rate</th>
+              <th />
+              <th />
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.movies.map(m => (
+              <tr key={m._id}>
+                <td>{m.title}</td>
+                <td>{m.genre.name}</td>
+                <td>{m.numberInStock}</td>
+                <td>{m.dailyRentalRate}</td>
+                <td>
+                  <Like liked={m.liked} likeToggle={() => this.handleLike(m)} />
+                </td>
+                <td>
+                  <button
+                    onClick={() => this.handleDelete(m)}
+                    className="btn btn-danger btn-sm"
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </React.Fragment>
+    );
   }
 }
 
